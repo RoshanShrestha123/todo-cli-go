95fafeff2bde90a8bcc9b93d7b58ac3af8155880
Roshan Shrestha <roshan.shrestha4u@gmail.com>
2025-02-22 12:54:43 +0545

    infinte loop logic, id type update

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/google/uuid"
 )
 
 type Task struct {
@@ -16,8 +14,8 @@ type Task struct {
 	IsCompleted bool   `json:"isCompleted"`
 }
 
-func createNewTask(title string) *Task {
-	return &Task{Id: uuid.New().ID(), Title: title, IsCompleted: false}
+func createNewTask(title string, len int) *Task {
+	return &Task{Id: uint32(len), Title: title, IsCompleted: false}
 
 }
 
@@ -36,45 +34,61 @@ func main() {
 		log.Fatal("Failed to load file")
 	}
 
-	var i int
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. List all task")
-	fmt.Println("2. Create new task")
-	fmt.Println("3. Delete task of id: ")
-	fmt.Scanln(&i)
+	for {
+		var i int
+		fmt.Println("What do you want to do?")
+		fmt.Println("=========================")
+		fmt.Println("1. List all task")
+		fmt.Println("2. Create new task")
+		fmt.Println("3. Delete task of id: ")
+		fmt.Scanln(&i)
 
-	switch i {
-	case 1:
-		fmt.Println("Here is the list of all the task")
-		for _, element := range allTodo {
-			fmt.Printf("%d - %s\n", element.Id, element.Title)
+		switch i {
+		case 1:
+			fmt.Println("Here is the list of all the task")
+			fmt.Println("________________________________")
+			for _, element := range allTodo {
+				fmt.Printf("%d - %s\n", element.Id, element.Title)
+			}
+
+		case 2:
+			fmt.Println("create new task")
+			scanner := bufio.NewScanner(os.Stdin)
+			scanner.Scan()
+			title := scanner.Text()
+
+			task := createNewTask(title, len(allTodo))
+			allTodo = append(allTodo, *task)
+
+			rawJson, err := json.Marshal(allTodo)
+			if err != nil {
+				log.Fatal("Can't create new task")
+			}
+
+			err = os.WriteFile("todo.json", rawJson, 0600)
+			if err != nil {
+				log.Fatal("Failed to write the data in file")
+			}
+
+			fmt.Println("<<Task has been created>>")
+
+		case 3:
+			fmt.Println("delete new task")
+
+		default:
+			fmt.Println("Invalid response")
+
 		}
 
-	case 2:
-		fmt.Println("create new task")
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		title := scanner.Text()
+		fmt.Println("You want to continue? Y/n")
+		var c string
+		fmt.Scan(&c)
 
-		task := createNewTask(title)
-		allTodo = append(allTodo, *task)
-
-		rawJson, err := json.Marshal(allTodo)
-		if err != nil {
-			log.Fatal("Can't create new task")
+		if c == "y" || c == "Y" {
+			continue
+		} else {
+			return
 		}
-
-		err = os.WriteFile("todo.json", rawJson, 0600)
-		if err != nil {
-			log.Fatal("Failed to write the data in file")
-		}
-
-	case 3:
-		fmt.Println("delete new task")
-
-	default:
-		fmt.Println("Invalid response")
-
 	}
 
 }
